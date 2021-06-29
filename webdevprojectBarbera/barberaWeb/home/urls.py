7c173d2c40e638dc80176976079d726b0c7adb94
Vishal Yadav <76230753+VishalIITP@users.noreply.github.com>
2021-06-29 12:33:32 +0530

    added signup and login

--- webdevprojectBarbera/barberaWeb/home/urls.py
+++ webdevprojectBarbera/barberaWeb/home/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     path('register', views.registerPage, name='registration'),
     path('login', views.loginPage, name='login'),
-
+    path('logedinhomepage', views.logedinhomepage, name="logedinhomepage"),
     path('polls', views.polls, name='polls'),
 
 ]
